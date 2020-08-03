c78ff0dca48136f9f93e28d1f02cfe14f5305d3d
davorin <47928470+davorinpiljic@users.noreply.github.com>
2020-08-02 20:28:02 -0600

    ring_buffer.py, reverse and names

--- names/names.py
+++ names/names.py
@@ -13,14 +13,22 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+names_1 = list(set(names_1))
+names_2 = list(set(names_2))
+merged_list = names_1 + names_2
+encountered = {}
+duplicates = []
+
+for name in merged_list:
+    if name in encountered:
+        encountered[name] += 1
+        duplicates.append(name)
+    else:
+        encountered[name] = 1
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
